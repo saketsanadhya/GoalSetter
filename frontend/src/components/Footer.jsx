9c293c922c405ce1f23b2e8058510f1372b50754
saketsanadhya <saketsanadhya2003@gmail.com>
2023-10-24 00:22:49 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p className='link'>Made by Saket Sanadhya</p>
+    </div>
+  )
+}
+
+export default Footer
